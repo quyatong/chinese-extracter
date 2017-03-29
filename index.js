7802542bdba7dd1514e12e97e61cf56af0dc5283
quyatong <quyatong@didichuxing.com>
2017-03-29 14:59:12 +0800

    修复一些问题

--- index.js
+++ index.js
@@ -1,79 +1,13 @@
 var fs = require('fs');
 var path = require('path');
 var readDir = require('readdir');
+var html = require('./lib/html');
+var js = require('./lib/js');
+var utils = require('./lib/utils');
 
+var zhReg = /[\u4e00-\u9fa5]+/g;
+var reg = /((([0-9]+(\.[0-9]+)?(\.[0-9]+)?)|[\u4e00-\u9fa5])+.*([,.，。：:；\-！\/￥%…？、]|([0-9]+(\.[0-9]+)?(\.[0-9]+)?)|[a-zA-Z\u4e00-\u9fa5])*)|(<%.*?%>)|(\{\$.*?\})/g;
 var all = [];
-
-/**
- * 去重函数
- *
- * @param  {Array} lines 行列表
- * @return {Array}       去重行
- */
-var unique = function (lines) {
-    var result = [];
-    var tmp = {};
-
-    lines.forEach(function (line) {
-        if (tmp[line.content]) {
-            if (tmp[line.content].linenum.indexOf(line.linenum) == -1) {
-                tmp[line.content].linenum.push(line.linenum);
-            }
-        }
-        else {
-            var obj = {
-                linenum: [line.linenum],
-                content: line.content,
-                origin: line.origin,
-                file: line.file,
-                project: line.project
-            };
-            tmp[line.content] = obj;
-            result.push(obj);
-        }
-    });
-
-    return result;
-};
-
-/**
- * 处理文件
- *
- * @param  {string} file  文件路径
- * @param  {Array}  lines 行列表
- * @return {Array}        语言列表
- */
-var handleFile = function (file, lines) {
-
-    var lang = [];
-    var content = fs.readFileSync(file, 'utf8').replace(/__i18n\((.*?)\)/g, function (match, content) {return content;});
-
-    lines.forEach(function (line) {
-        line.content.forEach(function (item) {
-            content = content.replace(item, '__i18n(' + item + ')');
-
-            lang.push({
-                zh: item,
-                en: ''
-            });
-        });
-    });
-
-    fs.writeFileSync(file, content);
-
-    return lang;
-};
-
-/**
- * 使用新行替换
- *
- * @param  {string} match
- * @return {string}       [description]
- */
-var replaceByNewLine = function (match) {
-    var lines = match.match(/\n/g);
-    return lines ? lines.join('') : '';
-};
 
 /**
  * 分析文件
@@ -85,32 +19,33 @@ var replaceByNewLine = function (match) {
 var analyzeFile = function (file, dir, projectPath) {
 
     var origin = fs.readFileSync(file, 'utf8');
-
     content = origin
     // 删除 // 注释
     .replace(/\/\/.*\n/g, '\n')
     // 删除 <!-- --> 注释
-    .replace(/<!--[\S\s]*?-->/g, replaceByNewLine)
+    .replace(/<!--[\S\s]*?-->/g, utils.replaceByNewLine)
     // 删除 /**/ 注释
-    .replace(/\/\*[\S\s]*?\*\//g, replaceByNewLine)
+    .replace(/\/\*[\S\s]*?\*\//g, utils.replaceByNewLine)
     // 删除 {**} 注释
     .replace(/\{\*.*?\*\}/g, '')
     .replace(/__i18n\((.*?)\)/g, function (match, content) {return content;})
     // 删除dd5
     .replace(/dd5/g, '')
-    .replace(/<\s*br\s*\/?\s*>/g, ' ')
+    .replace(/<\s*(br|img|meta|hr)[\s\S]*?>/g, utils.replaceByNewLine)
 
-    // 删除< div xxxx="xxxx" >标签
-    .replace(/<\s*?.*?\s*?>/g, '')
-    // 删除</div>标签
-    .replace(/<\s*?\/\s*.*?\s*?>/g, '');
+    // // 删除< div xxxx="xxxx" >标签
+    // .replace(/\n\s*<\s*?.*?\s*?>/g, replaceByNewLine)
+    // // 删除</div>标签
+    // .replace(/\n<\s*?\/\s*.*?\s*?>/g, replaceByNewLine)
+    // 删除console.log
+    .replace(/console\.log.*/ig, '')
+    .replace(/Console\.log.*/ig, '')
+    // 删除异常
+    .replace(/throw\s+.*/g, '');
 
 
     if (origin.split('\n').length != content.split('\n').length) {
-        console.log('--------------------------------------------------');
-        console.log(origin);
-        console.log('==================================================');
-        console.log(content);
+        throw new Error('程序出错');
     }
 
     var lines = [];
@@ -118,37 +53,31 @@ var analyzeFile = function (file, dir, projectPath) {
     origin = origin.split(/\n/g);
 
     content.split(/\n/g).forEach(function (line, linenum) {
+
+        if (!new RegExp(zhReg).test(line)) {
+            return;
+        }
+        // console.log(/\.(html|tpl|tmpl)$/g.test(file));
         var segs = [];
 
-        line.replace(
-            /((([0-9]+(\.[0-9]+)?(\.[0-9]+)?)|[\u4e00-\u9fa5，。：；（）\-！\/￥%…？、“”])+)|(<%.*?%>)|(\{\$.*?\})/g,
-            function (match) {
-                segs.push(match);
-            }
-        );
-
-        if (segs.length) {
-
-            segs = segs.filter(function (seg) {
-                if (/([\u4e00-\u9fa5]+)/g.test(seg)) {
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            });
-
-            if (segs.length) {
-                lines.push({
-                    linenum: (linenum + 1),
-                    content: segs,
-                    origin: origin[linenum],
-                    file: path.basename(projectPath) + file.replace(projectPath, ''),
-                    project: path.basename(projectPath)
-                });
-            }
+        // html
+        if(/\.(html|tpl|tmpl)$/g.test(file)) {
+            segs = html(line);
         }
+        else if (/\.(js)$/.test(file)) {
+            segs = js(line);
+        }
+        lines.push(
+            {
+                linenum: (linenum + 1),
+                content: segs,
+                origin: origin[linenum],
+                file: path.basename(projectPath) + file.replace(projectPath, ''),
+                project: path.basename(projectPath)
+            }
+        )
     });
+
     return lines;
 };
 
@@ -161,14 +90,50 @@ var analyzeFile = function (file, dir, projectPath) {
 var analyzePage = function (dir, projectPath) {
     var lang = [];
 
-    readDir.readSync(dir, ['**.js', '**.tmpl', '**.tpl', '**.html'], readDir.ABSOLUTE_PATHS)
+    readDir
+        .readSync(dir, ['**.js', '**.tmpl', '**.tpl', '**.html', '**.css'], readDir.ABSOLUTE_PATHS)
         .forEach(function (file) {
             var lines = analyzeFile(file, dir, projectPath);
-            all = all.concat(lines);
-            lang = lang.concat(handleFile(file, unique(lines)));
+
+            if (lines.length) {
+                all.push({
+                    project: path.basename(projectPath),
+                    page: file.replace(projectPath, '').split(/\//g).splice(1, 2).join('/'),
+                    lines: lines.map(function (line) {
+                        return line.content;
+                    })
+                });
+            }
+
+            lang = lang.concat(utils.handleFile(file, lines));
         });
 
-    fs.writeFileSync(dir + '/lang.json', JSON.stringify(lang, null, 2));
+    if (lang.length) {
+        fs.writeFileSync(dir + '/lang.json', JSON.stringify(lang, null, 2));
+    }
+};
+
+
+
+/**
+ * 分析页面
+ *
+ * @param  {string} dir         页面目录
+ * @param  {string} projectPath 工程目录
+ */
+var analyzePages = function (dir, projectPath) {
+    var lang = [];
+    readDir
+        .readSync(dir, ['*/'],
+            readDir.ABSOLUTE_PATHS + readDir.INCLUDE_DIRECTORIES + readDir.NON_RECURSIVE
+        )
+        .forEach(function (dir) {
+            analyzePage(dir, projectPath);
+        });
+
+    if (all.length) {
+        fs.writeFileSync(projectPath + '/lang.json', JSON.stringify(all, null, 2));
+    }
 };
 
 /**
@@ -186,9 +151,8 @@ var analyzeProject = function (projectPath) {
             readDir.ABSOLUTE_PATHS + readDir.INCLUDE_DIRECTORIES + readDir.NON_RECURSIVE
         )
         .forEach(function (dir) {
-            analyzePage(dir, projectPath);
+            analyzePages(dir, projectPath);
         });
-
 };
 
 /**
@@ -198,20 +162,25 @@ var analyzeProject = function (projectPath) {
  */
 exports.enter = function (program) {
     var cwd = process.cwd();
-    if (program.projects && program.projects.length) {
 
+    if (program.projects && program.projects.length) {
         program.projects.forEach(function (project) {
             analyzeProject(cwd + '/' + project);
         });
-
-        all = unique(all).map(function (item) {
-            return item.project + '\t' + '\t' + item.content.join(' ') + '\n'
-                + item.project + '\t' + '\t' + item.origin + '\n';
+        // all = unique(all).map(function (item) {
+        //     return item.project + '\t' + '\t' + item.content.join(' ') + '\n'
+        //         + item.project + '\t' + '\t' + item.origin + '\n';
+        // });
+    }
+    else if (program.files && program.files.length) {
+        program.files.forEach(function (file) {
+            var lines = analyzeFile(file, path.dirname(file), cwd);
+            utils.handleFile(file, lines);
         });
-
-        fs.writeFileSync(cwd + '/all.txt', all.join('\n'));
     }
     else {
         analyzeProject(cwd);
     }
+
+    fs.writeFileSync(cwd + '/lang.json', JSON.stringify(all, null, 2));
 };
